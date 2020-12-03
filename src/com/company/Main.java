a797f0501ed688edd778f9d3e9f5181fb1920092
isheva25 <XKGubjSQ1>
2020-12-03 23:21:13 +0200

    c2cbcccc47e2a333dc9db87955f469990737db3d

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
  class newproject {
 
     public static void main (String[] args) {
-      String novayazadacha = "Новый6666 ";
+      String novayazadacha = "Новый6888666 ";
       System.out.println(novayazadacha);}
  }
