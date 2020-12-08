7b1df758d33cba85824f0a807609afc4212748a0
Ilya Shevtsov <isheva25@gmail.com>
2020-12-08 21:53:44 +0200

    c2cbcccc47e2a333dc9db87955f469990737db3d

---
+++ src/com/company/Popitka2.java
@@ -0,0 +1,12 @@
+package com.company;
+import java.util.Scanner;
+
+public class Popitka2 {
+
+    public static void main(String[] args) {
+        System.out.println("Введите имя");
+        Scanner sc = new Scanner();
+        String imya = sc.nextLine();
+        System.out.println(imya);
+    }
+}
