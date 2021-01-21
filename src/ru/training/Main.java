668f7eef52bd7d8f4a3d6f37bef414a32183a730
Ilya Shevtsov <isheva25@gmail.com>
2021-01-21 21:49:54 +0200

    lesson 3

---
+++ src/ru/training/Main.java
@@ -0,0 +1,16 @@
+package ru.training;
+
+import java.util.Scanner;
+
+class lesson_3{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Доброго времени суток Бездельник");
+        System.out.println("Хочешь вычислить площадь какой-нибудь фигуры?");
+
+        System.out.println("Введите: Да/Нет");
+        String yesno = sc.nextLine();
+
+    }
+}
