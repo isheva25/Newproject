bbb37a26627be8639df0ee6f12e4bf806d4f2c68
Ilya Shevtsov <isheva25@gmail.com>
2021-01-23 23:25:50 +0200

    lesson 3

--- src/ru/training/Main.java
+++ src/ru/training/Main.java
@@ -8,9 +8,18 @@ class lesson_3{
 
         System.out.println("Доброго времени суток Бездельник");
         System.out.println("Хочешь вычислить площадь какой-нибудь фигуры?");
-
         System.out.println("Введите: Да/Нет");
+        String yes = "Да";
+        String no = "Нет";
+
         String yesno = sc.nextLine();
 
+        if (yesno=="Да") {
+            System.out.println(yes.equals(no));
+        }
+        else if (yesno=="Нет") {
+            System.out.println(yes.equals(no));
+
+        }
     }
 }
